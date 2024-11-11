b790427ab285e87297ba56ef4bfff0a3fa7857b1
atmabdullaham <atmabdullaham@gmail.com>
2024-11-11 20:52:26 +0600

    added the solutions for the finding even and odd numbers for some range

---
+++ task_02.js
@@ -0,0 +1,36 @@
+/***
+
+____________________________Task-02____________________________________
+
+Find all the odd numbers from 61 to 100.
+
+ */
+//____________Solutions___________________________________________
+
+let i = 60; //loop variable
+while (i <= 100) {
+ if (i % 2 !== 0) {
+  console.log(i)
+ }
+ i++;
+}
+
+
+// To find numbers from one to ten
+// let i = 0;
+// while (i <= 10) {
+//  console.log(i);
+//  i++;
+// }
+
+
+// find even number from 0 to 10;
+// let i = 0;
+// while (i <= 10) {
+//  if (i % 2 === 0) {
+//   console.log(i);
+//  }
+//  i++;
+// }
+
+
--- task_03.js
+++ task_03.js
@@ -7,3 +7,13 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// _____________________________Solutions__________________________
+
+let i = 78;//loop variable
+while (i <= 98) {
+ if (i % 2 === 0) {
+  console.log(i);
+ }
+ i++; // increment
+}
